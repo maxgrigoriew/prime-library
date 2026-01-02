8885364480a100e7dd8529e79fe7ff4c6015ca92
maksim.khlopyanik <maksim.khlopyanik@ural.rt.ru>
2026-01-02 21:35:02 +0300

    добавлен конфиг цветов tailwind

--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -1,3 +1,5 @@
+import {colors} from './src/assets/tailwind'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,7 +8,8 @@ export default {
   ],
   theme: {
     extend: {},
+    colors
   },
   plugins: [],
-  prefix: 'd-',
+  prefix: '',
 }
