81acd9fc4d75266ffb45d1c600f30f0eaa0b1e73
maksim.khlopyanik <maksim.khlopyanik@ural.rt.ru>
2026-01-07 12:41:40 +0300

    feat: добавлено плавное наведение на иконки todo

--- FRONTEND/src/entities/todos/ui/DodoList/TodoList.vue
+++ FRONTEND/src/entities/todos/ui/DodoList/TodoList.vue
@@ -18,29 +18,43 @@ type Props = {
 const {todos, modelValue} = defineProps<Props>()
 
 const emit = defineEmits<{
-  'update:modelValue' : [string]
+  'update:modelValue': [string]
   remove: [id: number]
   mark: [id: number]
   send: [void]
 }>()
 
-const updateNewTodoProxy = computed<string>( {
+const updateNewTodoProxy = computed<string>({
   get: () => modelValue,
   set: (value: string) => emit('update:modelValue', value)
 })
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-12 rounded-[8px] p-12 shadow-custom">
-    <div v-for="todo in todos" :key="todo.id">
-      <div class="flex p-4 gap-12 align-middle cursor-pointer border border-b-grey-2 rounded">
-        <input type="checkbox" class="cursor-pointer" :checked="todo.done" @change="emit('mark', todo.id)">
-        <div>{{ todo.text }}</div>
-        <IconTrash class="ml-auto" @click="emit('remove', todo.id)"/>
+  <div class="flex flex-col gap-y-12 rounded-[12px]">
+    <div class="flex gap-12 items-center">
+      <div class="bg-banana-2 w-32 h-32 flex justify-center items-center rounded-[4px]">
+        <IconTrash/>
+      </div>
+      <div class="font-medium">Мои задачи</div>
+    </div>
+
+    <div class="flex flex-col gap-[20px] overflow-y-auto">
+      <div v-for="todo in todos" :key="todo.id">
+        <div class="group flex items-center p-4 gap-12 cursor-pointer rounded-lg transition-colors duration-200">
+          <input type="checkbox" class="cursor-pointer" :checked="todo.done" @change="emit('mark', todo.id)">
+          <div class="flex-1">{{ todo.text }}</div>
+
+          <IconTrash
+              class="ml-auto opacity-0 group-hover:opacity-100 transition-all duration-200 cursor-pointer"
+              @click="emit('remove', todo.id)"
+          />
+        </div>
       </div>
     </div>
 
-    <div class="flex gap-12 items-center justify-between ">
+    <div class="h-1 bg-grey-2 mb-4"></div>
+    <div class="flex gap-12 items-center justify-between">
       <UiInput
           v-model="updateNewTodoProxy"
           placeholder="Добавьте задачу"
