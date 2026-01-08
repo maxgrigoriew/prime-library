26f147382308a37d8f468fbcbc6f1355a1c3ce19
maksim.khlopyanik <maksim.khlopyanik@ural.rt.ru>
2026-01-08 13:07:22 +0300

    fix ts: Доделать!

--- FRONTEND/vite.config.ts
+++ FRONTEND/vite.config.ts
@@ -1,53 +1,58 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
+import {fileURLToPath} from 'node:url';
+import {storybookTest} from '@storybook/addon-vitest/vitest-plugin';
+import {playwright} from '@vitest/browser-playwright';
 import svgLoader from 'vite-svg-loader'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [vue(), svgLoader({ defaultImport: 'component' }),],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
-    },
-  },
-  server: {
-    open: true,
-    host: 'localhost',
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        //target: 'https://crmpro-dev.rt.ru',
-        changeOrigin: true,
-        proxyTimeout: 30000,
-        timeout: 30000,
-      },
-    },
-  },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
+    plugins: [
+        vue(),
+        svgLoader({defaultImport: 'component'}),
+        tsconfigPaths()
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve('./src'),
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+    },
+    server: {
+        open: true,
+        host: 'localhost',
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                proxyTimeout: 30000,
+                timeout: 30000,
+            },
+        },
+    },
+    test: {
+        projects: [{
+            extends: true,
+            plugins: [
+                storybookTest({
+                    configDir: path.join(dirname, '.storybook')
+                })],
+            test: {
+                name: 'storybook',
+                browser: {
+                    enabled: true,
+                    headless: true,
+                    provider: playwright({}),
+                    instances: [{
+                        browser: 'chromium'
+                    }]
+                },
+                setupFiles: ['.storybook/vitest.setup.ts']
+            }
+        }]
+    }
 });
