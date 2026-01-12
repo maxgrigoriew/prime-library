f723abb37349afcdfb0341c7c7d5f41fb1300956
Maxim Khlopyanik <sss123123111@gmail.com>
2026-01-12 11:52:35 +0300

    feat: добавлено позиционирование в tooltip

--- FRONTEND/src/pages/TaskPage/TaskPage.vue
+++ FRONTEND/src/pages/TaskPage/TaskPage.vue
@@ -5,11 +5,65 @@ import UiTestTooltip from "@/ui/UiTestTooltip/UiTestTooltip.vue";
 </script>
 
 <template>
-   <UiTestTooltip>
-     <template #trigger>
-       <UiButton>Наведи на меня</UiButton>
-     </template>
-   </UiTestTooltip>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <UiTestTooltip :by-click="false">
+    <template #trigger>
+      <UiButton>Наведи на меня</UiButton>
+    </template>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolore eligendi fugiat odio quaerat repellat sed tenetur ullam? Blanditiis cum eos eveniet harum, impedit ipsa magni molestias pariatur quis ullam?
+  </UiTestTooltip>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br><br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
 </template>
 
 <style scoped>
