03dfc470071c99ffb5bb71ac9c3af593301b7844
maksim.khlopyanik <maksim.khlopyanik@ural.rt.ru>
2026-01-18 18:07:49 +0300

    feat: шапка сделки

--- FRONTEND/src/pages/DealListPage/DealListPage.vue
+++ FRONTEND/src/pages/DealListPage/DealListPage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 
+import {UiButton, UiInput} from "@/ui";
+
 defineOptions({
   name: 'DealListPage'
 })
@@ -7,7 +9,20 @@ defineOptions({
 </script>
 
 <template>
-  <div>DealListPage page...</div>
+  <div class="flex gap-12 flex-col">
+    <div class="flex items-center justify-between font-medium gap-12">
+      <div class="text-2xl">Список сделок</div>
+      <UiButton>Моздать сделку</UiButton>
+    </div>
+
+    <div class="flex gap-12">
+      <UiInput class="w-50%" placeholder="Поиск по организации"/>
+      <UiInput class="w-50%" placeholder="Поиск по сделке"/>
+    </div>
+
+    <div>Список фильтров...</div>
+
+  </div>
 </template>
 
 <style scoped>
