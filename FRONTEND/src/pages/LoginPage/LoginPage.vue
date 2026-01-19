05542c240ed77aeb040fac36ed7eefbe071152f3
Maxim Khlopyanik <sss123123111@gmail.com>
2026-01-19 15:27:31 +0300

    feat: добавил библиотеку shadcn

--- FRONTEND/src/pages/LoginPage/LoginPage.vue
+++ FRONTEND/src/pages/LoginPage/LoginPage.vue
@@ -4,6 +4,7 @@ import UiAlert from "@/ui/UiAlert/UiAlert.vue";
 import UiInput from "@/ui/UiInput/UiInput.vue";
 import UiButton from "@/ui/UiButton/UiButton.vue";
 import {authFt} from "@/entities/auth/features/authFf.ts";
+import Button from "@/components/ui/button/Button.vue";
 
 defineOptions({
   name: 'LoginPage'
@@ -15,7 +16,7 @@ const {userLogin, userPassword ,loadMyProfile} = authFt()
 
 <template>
   <div class="h-1 bg-gray-200"></div>
-
+<Button >button</Button>
   <div class="px-12 m-auto flex flex-col gap-20">
     <div class="px-32 text-2xl font-bold ">Вход на сайт</div>
     <UiAlert type="secondary">
