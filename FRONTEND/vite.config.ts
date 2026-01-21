829d26610602c8b3cd42ab3ebb07e59604445c80
Maxim Khlopyanik <sss123123111@gmail.com>
2026-01-21 11:19:14 +0300

    fix: test

--- FRONTEND/vite.config.ts
+++ FRONTEND/vite.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
+
 import {storybookTest} from '@storybook/addon-vitest/vitest-plugin';
+import vue from '@vitejs/plugin-vue';
 import {playwright} from '@vitest/browser-playwright';
+import {defineConfig} from 'vite';
 import svgLoader from 'vite-svg-loader'
 
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
